ef13d45ef61471afcfa1494c41cd99a4d2856d02
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-09-07 16:11:49 +0545

    Divided Entire Design into three sections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+// Import components
+import Sidebar from "./components/Sidebar";
+import InsideSidebar from "./components/InsideSidebar";
+import Main from "./components/Main";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex flex-row">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Second Sidebar */}
+      <InsideSidebar />
+      {/* Main*/}
+      <Main />
+    </div>
+  );
 };
 
 export default App;
