a2584f20e06f0161d2b43d964604af1c4c0b675e
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-09-07 11:01:42 +0545

    Some theme added inside tailwind config file

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,6 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      primary: "Rubik",
+    },
+    colors: {
+      dark: {
+        DEFAULT: "#222831",
+      },
+      light_dark: {
+        DEFAULT: "#393E46",
+      },
+      teal: {
+        DEFAULT: "#00ADB5",
+      },
+      violet: "#2B076E",
+      white: "#EEEEEE",
+    },
+    boxShadow: {
+      primary: "0px 20px 200px rgba(57, 23, 119, 0.05)",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
     extend: {},
   },
   plugins: [],
