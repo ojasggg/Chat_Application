31516aa38f8aacdfafac3042c58ff25166ef5d50
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-09-08 14:54:02 +0545

    Mid Messages Section is Fininshed

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -39,6 +39,62 @@ const Main = () => {
           </li>
         </ul>
       </header>
+
+      {/* Mid */}
+      <div>
+        <div>
+          <h3 className="p text-center mt-2">Today</h3>
+        </div>
+
+        {/* Left or Other Message */}
+        <div className="my-7">
+          <div className="relative ml-3 bg-light_dark rounded-xl rounded-tl-none max-w-[300px] p-4 cursor-pointer">
+            <p className="p ml-5 text-[15px] text-white/60">
+              Wanna travel to Japan tomorrow?
+            </p>
+            <div className="absolute bottom-[-20px] right-3">
+              <p className="h6">12.02 PM</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right or Self Message */}
+        <div className="flex flex-col items-end w-full my-7 ">
+          <div className="relative mr-3 bg-teal rounded-xl rounded-tr-none max-w-[300px] p-3 cursor-pointer">
+            <p className="p ml-2 text-[15px] text-black/70">
+              Sorry man I gotta go to Dubai to meet prince, You continue I will
+              Catch up with you in tokyo.
+            </p>
+            <div className="absolute bottom-[-20px]">
+              <p className="h6">12.05 PM</p>
+            </div>
+          </div>
+        </div>
+        {/* Left or Other Message */}
+        <div className="my-7">
+          <div className="relative ml-3 bg-light_dark rounded-xl rounded-tl-none max-w-[300px] p-4 cursor-pointer">
+            <p className="p ml-5 text-[15px] text-white/60">
+              Wanna travel to Japan tomorrow?
+            </p>
+            <div className="absolute bottom-[-20px] right-3">
+              <p className="h6">12.02 PM</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right or Self Message */}
+        <div className="flex flex-col items-end w-full my-7 ">
+          <div className="relative mr-3 bg-teal rounded-xl rounded-tr-none max-w-[300px] p-3 cursor-pointer">
+            <p className="p ml-2 text-[15px] text-black/70">
+              Sorry man I gotta go to Dubai to meet prince, You continue I will
+              Catch up with you in tokyo.
+            </p>
+            <div className="absolute bottom-[-20px]">
+              <p className="h6">12.05 PM</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
