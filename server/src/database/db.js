b39f378f6f64149e5d5432be7e24d768ee5f9cfe
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-09-22 15:23:16 +0545

    Database connection is established with the online cluster

---
+++ server/src/database/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongodb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("monogdb disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("monogdb connected");
+});
+
+module.exports = connect;
