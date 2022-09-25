e78886107b3ad571a11e8de468f1733c55570542
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-09-26 00:37:31 +0545

    Error handling middle ware created, cookieparser added and authentication router added

--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const db = require("./src/database/db");
 const conversationRoute = require("./src/routes/conversation.router");
+const authenticationRoute = require("./src/routes/auth.router");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Middle ware
 app.use("/api/conversation", conversationRoute);
+app.use("/api/auth", authenticationRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something Went Wrong!";
+
+  res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 app.listen(process.env.PORT || 90, () => {
   db();
