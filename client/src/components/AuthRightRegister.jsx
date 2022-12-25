7c1f8b769c6dea0e48892bec24e8ad0e82ca9eaf
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-12-25 18:14:41 +0545

    Register right side page, same not required in separated component

---
+++ client/src/components/AuthRightRegister.jsx
@@ -0,0 +1,9 @@
+import RegisterForm from "./RegisterForm";
+
+export const AuthRightRegister = () => {
+  return (
+    <div className="flex-1 h-screen">
+      <RegisterForm />
+    </div>
+  );
+};
