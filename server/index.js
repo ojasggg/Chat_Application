c0a9b362d679e7fe0afccc83495684714b210608
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-12-25 18:12:21 +0545

    CORS and credentials properties added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+  })
+);
 
 // Middle ware
 app.use("/api/conversation", conversationRoute);
