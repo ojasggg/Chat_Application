1e240e4575b7836d04c0cec75d733a247f3b3adb
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-12-29 23:42:26 +0545

    Get user props from App.jsx to distribute into other component(prop drilling is the problem because of the logic needed to be applied.)

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 import Chat from "../components/Chat";
 import InsideSidebar from "../components/InsideSidebar";
 import Sidebar from "../components/Sidebar";
 
-import { useCookies } from "react-cookie";
-import Cookies from "universal-cookie";
+export const Home = ({ user }) => {
+  const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  useEffect(() => {
+    const getConversation = async () => {
+      try {
+        const chat = await Axios.get(`/api/conversation/${user?._id}`);
+        setConversations(chat.data.conversation);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getConversation();
+  }, [user]);
 
-export const Home = () => {
-  const [cookies, setCookies, removeCookies] = useCookies();
-  console.log(cookies["access_token"]);
   return (
     <div className="flex flex-row h-full">
       <Sidebar />
-      <InsideSidebar />
-      <Chat />
+      <InsideSidebar
+        conversations={conversations}
+        user={user}
+        setCurrentChat={setCurrentChat}
+      />
+      <Chat currentChat={currentChat} currentUserId={user._id} />
     </div>
   );
 };
